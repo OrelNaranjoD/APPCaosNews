8019c8ea009196c00990b3db6f529d45c4ad4211
Orel Naranjo <or.naranjo@duocuc.cl>
2023-05-27 12:59:48 -0400

    Agragada tabla noticias

--- CaosNewsApp/models.py
+++ CaosNewsApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Noticia(models.Model):
+    id_noticia  = models.AutoField(db_column='id_noticia', primary_key=True) 
+    titulo_noticia = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.titulo_noticia)
--- CaosNewsApp/views.py
+++ CaosNewsApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Noticia
 
-# Create your views here.
+def index(request):
+    noticias = Noticia.objects.raw('SELECT * FROM CaosNewsAPP_Noticia')
+    print(noticias)
+    context={"noticias":noticias}
+    return render(request, 'index.html', context)
