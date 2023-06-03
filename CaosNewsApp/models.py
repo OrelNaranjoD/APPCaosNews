e47cc64fd7d6481e9454ca9025decea90ac5df9e
diego <die.marinf@duocuc.cl>
2023-06-03 13:07:34 -0400

    Agregado roles de usuario

--- CaosNewsApp/models.py
+++ CaosNewsApp/models.py
@@ -1,15 +1,85 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db.models.deletion import SET_NULL
 
 class Noticia(models.Model):
-    id_noticia  = models.AutoField(db_column='id_noticia', primary_key=True) 
-    titulo_noticia = models.CharField(max_length=20, blank=False, null=False)
-    subtitulo_noticia = models.CharField(max_length=50, blank=False, null=False)
-    cuerpo_noticia = models.CharField(max_length=999999, blank=False, null=False)
-    activo = models.BooleanField(default='000000')
+    id_noticia = models.AutoField(primary_key=True)
+    titulo_noticia = models.CharField(max_length=20, default='')
+    subtitulo_noticia = models.CharField(max_length=50, default='')
+    cuerpo_noticia = models.CharField(max_length=500, default='')
+    activo = models.BooleanField(default=True)
 
     def __str__(self):
-        return str(self.titulo_noticia)
-   
+        return self.titulo_noticia
+
+    # Opcional: añadir los Meta options según tus necesidades
+    class Meta:
+        verbose_name = 'Noticia'
+        verbose_name_plural = 'Noticias'
+    
+class CustomUser(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('admin'  , 'Administrador' ),
+        ('editor'  , 'Editor'),
+        ('periodista', 'Periodista'),
+        ('colaborador', 'Colaborador'),
+        ('suscriptor' ,'Suscriptor'),
+
+    )
+    
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, default='suscriptor' )
+
+
+ # Asegúrate de que no haya conflicto de nombres con 'auth.Group.user'
+    user_groups = models.ManyToManyField(
+        Group,
+        related_name='custom_users',
+        related_query_name='custom_user',
+        blank=True,
+        verbose_name='User groups'
+    )
+    
+    # Asegúrate de que no haya conflicto de nombres con 'auth.User.groups'
+    custom_user_groups = models.ManyToManyField(
+        'self',
+        related_name='group_members',
+        related_query_name='group_member',
+        symmetrical=False,
+        blank=True,
+        verbose_name='Custom user groups'
+    )
+    
+    # Asegúrate de que no haya conflicto de nombres con 'auth.User.user_permissions'
+    custom_user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_users',
+        related_query_name='custom_user',
+        blank=True,
+        verbose_name='Custom user permissions'
+    )
+    
+    # Asegúrate de que no haya conflicto de nombres con 'auth.User.groups'
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_users_set',
+        related_query_name='custom_user_set',
+        blank=True,
+        verbose_name='Groups',
+        help_text='The groups this user belongs to.',
+        symmetrical=False,
+    )
+
+    # Asegúrate de que no haya conflicto de nombres con 'auth.User.user_permissions'
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_users_set',
+        related_query_name='custom_user_set',
+        blank=True,
+        verbose_name='User permissions',
+        help_text='Specific permissions for this user.',
+        symmetrical=False,
+    )
 '''class Alumno(models.Model):
     rut = models.CharField(primary_key=True, max_length=10)
     nombre = models.CharField(max_length=20)
