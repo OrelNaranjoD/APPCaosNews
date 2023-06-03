f511c3c51a3bf8bdb74f2b837dba8ec05d7d13b0
Orel Naranjo <or.naranjo@duocuc.cl>
2023-06-03 12:08:59 -0400

    Agregado GitIgnore para eliminar cache

--- CaosNewsApp/models.py
+++ CaosNewsApp/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Noticia(models.Model):
     id_noticia  = models.AutoField(db_column='id_noticia', primary_key=True) 
     titulo_noticia = models.CharField(max_length=20, blank=False, null=False)
+    subtitulo_noticia = models.CharField(max_length=50, blank=False, null=False)
+    cuerpo_noticia = models.CharField(max_length=999999, blank=False, null=False)
+    activo = models.BooleanField(default='000000')
 
     def __str__(self):
         return str(self.titulo_noticia)
